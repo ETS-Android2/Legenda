fbce2a681555cdb04b8112bd7698bce363a18ec1
DanStep65 <danielstephen65@gmail.com>
2020-11-17 20:23:43 +0700

    Updating the xml for every game screen that should only has a "next" button to press.

--- app/src/main/java/com/example/legenda/GameScreenYudis12.java
+++ app/src/main/java/com/example/legenda/GameScreenYudis12.java
@@ -2,7 +2,9 @@ package com.example.legenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameScreenYudis12 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class GameScreenYudis12 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen_yudis12);
+    }
+    public void selectChoice2(View view){//MoveLayout to next GameScreen c2
+        Intent intent = new Intent(GameScreenYudis12.this, GameScreenYudis3.class);
+        startActivity(intent);
     }
 }
